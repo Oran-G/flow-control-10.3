856f75b951c7d837e00761da153ed3b46a64ff37
Oran-G <31713576+Oran-G@users.noreply.github.com>
2020-11-16 21:07:16 -0500

    Create q4.cpp

---
+++ q4.cpp
@@ -0,0 +1,25 @@
+void setup() { 
+    Serial.begin(9600); 
+    pinMode(13, OUTPUT); 
+} 
+void loop() { 
+    if (analogRead(A0) > 600) 
+    { 
+        digitalWrite(13, HIGH); 
+    } 
+    else 
+    { 
+        if (n >= 512) {
+            digitalWrite(13, LOW); 
+        }
+        else if (n <= 1024)
+        {
+            n = 0
+        }
+        else
+        {
+          digitalWrite(13, HIGH)
+        }
+          
+    } 
+} 
