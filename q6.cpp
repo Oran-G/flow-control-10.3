ab0f31c0270a72ee0443264e858e0bd2f237b2d6
Oran-G <31713576+Oran-G@users.noreply.github.com>
2020-11-16 21:13:45 -0500

    Create q6.cpp

---
+++ q6.cpp
@@ -0,0 +1,30 @@
+int n = 0;
+void setup() { 
+    Serial.begin(9600); 
+    pinMode(13, OUTPUT); 
+    
+} 
+void loop() { 
+    if (analogRead(A0) < 100 || analogRead(A0) > 950)
+    { 
+        digitalWrite(13, HIGH); 
+    } 
+    else 
+    { 
+        if (n <= 512) {
+            digitalWrite(13, LOW); 
+        }
+        else if (n <= 1024)
+        {
+            n = 0;
+        }
+        else
+        {
+          digitalWrite(13, HIGH);
+        }
+          
+    }
+    delay(1);
+    n++;
+     
+} 
